b3235d4fbc09937a15a183cf0a920a3384d9b3f3
T-Cuong <trongcuong17pfiev@gmail.com>
2023-02-03 10:35:56 +0700

    e_web

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+     <section className='head'>
+        <div className='container d_flex'>
+            <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label htmlFor=''>0332034119</label>
+            <i className='fa fa-envelope'></i>
+            <label htmlFor=''>trongcuong17pfiev@gmail.com</label>
+            </div>
+            <div className='right row RText'>
+                <label>Theme FAQ's</label>
+                <label>Theme FAQ's</label>
+                <span>🏴</span>
+                <label htmlFor=''>EN</label>
+                <span>🏴</span>
+                <label htmlFor=''>USA</label>
+            </div>
+        </div>
+     </section>
+    </>
+  )
+}
+
+export default Head
